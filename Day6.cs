db52e0424d950b1ff9513242cc8f766b857ef851
Will Smith <will@toxon.co.uk>
2020-12-06 08:04:57 +0000

    Solve Day6 Part2

--- Day6.cs
+++ Day6.cs
@@ -15,9 +15,9 @@ namespace AdventOfCode2020
         private static readonly string Sample = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb";
 
         private static readonly TextParser<char> Answer = Character.Lower;
-        private static readonly TextParser<char[]> PersonAnswer = Answer.AtLeastOnce();
-        private static readonly TextParser<HashSet<char>> AnswerGroup = PersonAnswer.Try().AtLeastOnceDelimitedBy(SuperpowerExtensions.NewLine).Select(x => x.SelectMany(x => x).ToHashSet());
-        private static readonly TextParser<HashSet<char>[]> AnswerGroups = AnswerGroup.Try().AtLeastOnceDelimitedBy(SuperpowerExtensions.NewLine.IgnoreThen(SuperpowerExtensions.NewLine));
+        private static readonly TextParser<HashSet<char>> PersonAnswer = Answer.AtLeastOnce().Select(x => x.ToHashSet());
+        private static readonly TextParser<HashSet<char>[]> AnswerGroup = PersonAnswer.Try().AtLeastOnceDelimitedBy(SuperpowerExtensions.NewLine);
+        private static readonly TextParser<HashSet<char>[][]> AnswerGroups = AnswerGroup.Try().AtLeastOnceDelimitedBy(SuperpowerExtensions.NewLine.IgnoreThen(SuperpowerExtensions.NewLine));
 
         private readonly ITestOutputHelper _output;
 
@@ -37,10 +37,36 @@ namespace AdventOfCode2020
             _output.Run("actual", () => GetUniqueAnswers(data));
         }
 
+        [Fact]
+        public void Part2()
+        {
+            var data = LoadData("day6");
+
+            _output.Run("sample", () => GetDistinctAnswers(Sample))
+                .Should().Be(6);
+
+            _output.Run("actual", () => GetDistinctAnswers(data));
+        }
+
         private static int GetUniqueAnswers(string data)
         {
             var answerGroups = AnswerGroups.MustParse(data);
-            return answerGroups.Sum(x => x.Count);
+
+            return answerGroups.Sum(CountGroup);
+
+            static int CountGroup(HashSet<char>[] people) => people.SelectMany(x => x).ToHashSet().Count;
+        }
+
+        private static int GetDistinctAnswers(string data)
+        {
+            var answerGroups = AnswerGroups.MustParse(data);
+
+            return answerGroups.Sum(CountGroup);
+
+            int CountGroup(HashSet<char>[] people)
+            {
+                return people.SelectMany(x => x).GroupBy(x => x).Count(x => x.Count() == people.Length);
+            }
         }
 
         private static string LoadData(string fileName) =>
