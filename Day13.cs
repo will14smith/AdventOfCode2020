b908780371dcd464dc6767067afe0e6433db9276
Will Smith <will@toxon.co.uk>
2020-12-13 12:15:28 +0000

    Solve Day13 Part2

--- Day13.cs
+++ Day13.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using AdventOfCode2020.Utilities;
 using FluentAssertions;
 using Xunit;
@@ -21,10 +22,22 @@ namespace AdventOfCode2020
             Run("actual", Parse, SolvePart1);
         }
 
-        private static long SolvePart1((int TimeToWait, IReadOnlyCollection<int> Buses) input)
+        [Fact]
+        public void Part2()
         {
-            var bestBus = 0;
-            var bestTime = int.MaxValue;
+            Run("sample", Sample, Parse, SolvePart2).Should().Be(1068781);
+            Run("sample2", "0\n17,x,13,19\n", Parse, SolvePart2).Should().Be(3417);
+            Run("sample3", "0\n67,7,59,61\n", Parse, SolvePart2).Should().Be(754018);
+            Run("sample4", "0\n67,x,7,59,61\n", Parse, SolvePart2).Should().Be(779210);
+            Run("sample5", "0\n67,7,x,59,61\n", Parse, SolvePart2).Should().Be(1261476);
+            Run("sample6", "0\n1789,37,47,1889\n", Parse, SolvePart2).Should().Be(1202161486);
+            Run("actual", Parse, SolvePart2);
+        }
+
+        private static long SolvePart1((long TimeToWait, IReadOnlyList<long> Buses) input)
+        {
+            var bestBus = 0L;
+            var bestTime = long.MaxValue;
 
             foreach (var bus in input.Buses.Where(x => x != -1))
             {
@@ -39,12 +52,49 @@ namespace AdventOfCode2020
             return bestBus * bestTime;
         }
 
-        private static (int TimeToWait, IReadOnlyCollection<int> Buses) Parse(string input)
+        private static long SolvePart2((long _, IReadOnlyList<long> Buses) input)
+        {
+            var schedule = input.Buses.Select((x, i) => (Bus: x, Offset: (long)i)).Where(x => x.Bus != -1).ToList();
+
+            return SolvePart2(schedule);
+        }
+
+        private static long SolvePart2(IReadOnlyList<(long Bus, long Offset)> schedule)
+        {
+            var (bus0, offset0) = schedule[0];
+
+            for (var index = 1; index < schedule.Count; index++)
+            {
+                var (busI, offsetI) = schedule[index];
+
+                // bus0 and busI will meet once in the busN period
+                var busN = bus0 * busI;
+
+                // calculate how many times bus0 has to visit before we line up
+                var offsetN = offset0;
+                while (offsetN < busN && (offsetN + offsetI) % busI != 0)
+                {
+                    offsetN += bus0;
+                }
+
+                if (offsetN > busN)
+                {
+                    throw new Exception("buses never align");
+                }
+
+                bus0 = busN;
+                offset0 = offsetN;
+            }
+
+            return offset0;
+        }
+
+        private static (long TimeToWait, IReadOnlyList<long> Buses) Parse(string input)
         {
             var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
-            var timeToWait = int.Parse(lines[0]);
-            var buses = lines[1].Split(',').Select(x => x != "x" ? int.Parse(x) : -1).ToList();
+            var timeToWait = long.Parse(lines[0]);
+            var buses = lines[1].Split(',').Select(x => x != "x" ? long.Parse(x) : -1).ToList();
 
             return (timeToWait, buses);
         }
