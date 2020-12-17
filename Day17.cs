b2e23f9a37a6dc725d3920f10aee11e55894420c
Will Smith <will@toxon.co.uk>
2020-12-17 05:48:12 +0000

    Solve Day17 Part2

--- Day17.cs
+++ Day17.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using AdventOfCode2020.Utilities;
 using FluentAssertions;
 using Superpower;
@@ -27,14 +26,24 @@ namespace AdventOfCode2020
             Run("sample", Sample, Slice, SolvePart1).Should().Be(112);
             Run("actual", Slice, SolvePart1);
         }
+        
+        [Fact]
+        public void Part2()
+        {
+            Run("sample", Sample, Slice, SolvePart2).Should().Be(848);
+            Run("actual", Slice, SolvePart2);
+        }
 
-        private int SolvePart1(bool[][] input)
+        private static int SolvePart1(bool[][] input) => Solve(input, false);
+        private static int SolvePart2(bool[][] input) => Solve(input, true);
+        
+        private static int Solve(bool[][] input, bool includeW)
         {
             var state = BuildInitialState(input);
 
             for (var i = 0; i < 6; i++)
             {
-                state = Step(state);
+                state = Step(state, includeW);
             }
 
             return state.Active.Count;
@@ -50,7 +59,7 @@ namespace AdventOfCode2020
                 {
                     if (!input[y][x]) continue;
                     
-                    var loc = new Loc(x, y, 0);
+                    var loc = new Loc(x, y, 0, 0);
                     active.Add(loc);
                 }
             }
@@ -58,28 +67,34 @@ namespace AdventOfCode2020
             return new State(active);
         }
         
-        private static State Step(State state)
+        private static State Step(State state, bool includeW)
         {
             var next = new HashSet<Loc>(state.Active);
 
-            for (var z = state.Min.Z - 1; z <= state.Max.Z + 1; z++)
+            var minW = includeW ? state.Min.W - 1 : 0;
+            var maxW = includeW ? state.Max.W + 1 : 0;
+            
+            for (var w = minW; w <= maxW; w++)
             {
-                for (var y = state.Min.Y - 1; y <= state.Max.Y + 1; y++)
+                for (var z = state.Min.Z - 1; z <= state.Max.Z + 1; z++)
                 {
-                    for (var x = state.Min.X - 1; x <= state.Max.X + 1; x++)
+                    for (var y = state.Min.Y - 1; y <= state.Max.Y + 1; y++)
                     {
-                        var loc = new Loc(x, y, z);
+                        for (var x = state.Min.X - 1; x <= state.Max.X + 1; x++)
+                        {
+                            var loc = new Loc(x, y, z, w);
 
-                        var isActive = state.Active.Contains(loc);
-                        var count = state.CountAdjacent(loc);
-                        
-                        if (!(count == 2 || count == 3) && isActive)
-                        {
-                            next.Remove(loc);
-                        }
-                        else if (count == 3 && !isActive)
-                        {
-                            next.Add(loc);
+                            var isActive = state.Active.Contains(loc);
+                            var count = state.CountAdjacent(loc);
+                            
+                            if (!(count == 2 || count == 3) && isActive)
+                            {
+                                next.Remove(loc);
+                            }
+                            else if (count == 3 && !isActive)
+                            {
+                                next.Add(loc);
+                            }
                         }
                     }
                 }
@@ -88,7 +103,7 @@ namespace AdventOfCode2020
             return new State(next);
         }
         
-        private record Loc(int X, int Y, int Z);
+        private record Loc(int X, int Y, int Z, int W);
         private class State
         {
             public State(ISet<Loc> active)
@@ -107,18 +122,21 @@ namespace AdventOfCode2020
 
             public int CountAdjacent(Loc loc)
             {
-                var (x, y, z) = loc;
+                var (x, y, z, w) = loc;
                 var count = 0;
-                
-                for (var dz = -1; dz <= 1; dz++)
-                {
-                    for (var dy = -1; dy <= 1; dy++)
-                    {
-                        for (var dx = -1; dx <= 1; dx++)
-                        {
-                            if (dz == 0 && dy == 0 && dx == 0) continue;
 
-                            count += Active.Contains(new Loc(x + dx, y + dy, z + dz)) ? 1 : 0;
+                for (var dw = -1; dw <= 1; dw++)
+                {
+                    for (var dz = -1; dz <= 1; dz++)
+                    {
+                        for (var dy = -1; dy <= 1; dy++)
+                        {
+                            for (var dx = -1; dx <= 1; dx++)
+                            {
+                                if (dw == 0 && dz == 0 && dy == 0 && dx == 0) continue;
+
+                                count += Active.Contains(new Loc(x + dx, y + dy, z + dz, w + dw)) ? 1 : 0;
+                            }
                         }
                     }
                 }
@@ -128,8 +146,8 @@ namespace AdventOfCode2020
             
             private static (Loc Min, Loc Max) CalculateBounds(ISet<Loc> active)
             {
-                var min = new Loc(int.MaxValue, int.MaxValue, int.MaxValue);
-                var max = new Loc(0, 0, 0);
+                var min = new Loc(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue);
+                var max = new Loc(int.MinValue, int.MinValue, int.MinValue, int.MinValue);
 
                 foreach (var loc in active)
                 {
@@ -141,9 +159,9 @@ namespace AdventOfCode2020
 
             private static void UpdateBounds(ref Loc min, ref Loc max, Loc loc)
             {
-                var (x, y, z) = loc;
-                min = min with { X = Math.Min(x, min.X), Y = Math.Min(y, min.Y), Z = Math.Min(z, min.Z) };
-                max = max with { X = Math.Max(x, max.X), Y = Math.Max(y, max.Y), Z = Math.Max(z, max.Z) };
+                var (x, y, z, w) = loc;
+                min = min with { X = Math.Min(x, min.X), Y = Math.Min(y, min.Y), Z = Math.Min(z, min.Z), W = Math.Min(w, min.W) };
+                max = max with { X = Math.Max(x, max.X), Y = Math.Max(y, max.Y), Z = Math.Max(z, max.Z), W = Math.Max(w, max.W) };
             }
         }
     }
