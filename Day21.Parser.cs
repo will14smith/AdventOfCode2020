690c547f9bc02354b2d7121f6a6df14aa40993fe
Will Smith <will@toxon.co.uk>
2020-12-21 09:33:15 +0000

    Day21 Part1

---
+++ Day21.Parser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2020.Utilities;
+using Superpower;
+using Superpower.Parsers;
+using Superpower.Tokenizers;
+
+namespace AdventOfCode2020
+{
+    public partial class Day21
+    {
+
+        private static readonly Tokenizer<TokenType> Tokenizer = new TokenizerBuilder<TokenType>()
+            .Ignore(Span.WhiteSpace)
+            .Match(Span.EqualTo("contains"), TokenType.Contains)
+            .Match(Character.Letter.AtLeastOnce(), TokenType.Identifier)
+            .Match(Character.EqualTo('('), TokenType.LeftBracket)
+            .Match(Character.EqualTo(','), TokenType.Comma)
+            .Match(Character.EqualTo(')'), TokenType.RightBracket)
+            .Build();
+
+        private static readonly TokenListParser<TokenType, string[]> Contains =
+            Token.Sequence(TokenType.LeftBracket, TokenType.Contains)
+                .IgnoreThen(Token.EqualTo(TokenType.Identifier).Select(x => x.ToStringValue()).AtLeastOnceDelimitedBy(Token.EqualTo(TokenType.Comma)))
+                .ThenIgnore(Token.EqualTo(TokenType.RightBracket));
+        
+        private static readonly TokenListParser<TokenType, Line> LineParser = Token.EqualTo(TokenType.Identifier).Select(x => x.ToStringValue()).AtLeastOnce().Then(Contains, (ingredients, allergens) => new Line(ingredients, allergens)); 
+        private static readonly TokenListParser<TokenType, Line[]> Parser = LineParser.AtLeastOnce(); 
+
+        private record Line(IReadOnlyList<string> Ingredients, IReadOnlyList<string> Allergens) { }
+
+        private enum TokenType
+        {
+            Contains,
+            Identifier,
+            LeftBracket,
+            Comma,
+            RightBracket,
+        }
+    }
+}
---
+++ Day21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2020.Utilities;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020
+{
+    public partial class Day21 : Test
+    {
+        private const string Sample = "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)\ntrh fvjkl sbzzf mxmxvkd (contains dairy)\nsqjhc fvjkl (contains soy)\nsqjhc mxmxvkd sbzzf (contains fish)\n";
+        
+        public Day21(ITestOutputHelper output) : base(21, output) { }
+
+        [Fact]
+        public void Part1()
+        {
+            Run("sample", Sample, Tokenizer, Parser, SolvePart1).Should().Be(5);
+            Run("actual", Tokenizer, Parser, SolvePart1);
+        }
+
+        private static long SolvePart1(IReadOnlyList<Line> input)
+        {
+            // some or all of the allergens the food contains (allergens aren't always marked)
+            // each allergen is found in exactly one ingredient
+            // each ingredient contains zero or one allergen
+            
+            var grouped = input.SelectMany(x => x.Allergens.Select(a => (a, x.Ingredients))).GroupBy(x => x.a).ToDictionary(x => x.Key, x => x.Select(l => l.Ingredients).Aggregate((a, i) => a.Intersect(i).ToList()));
+
+            var possibleIngredients = grouped.SelectMany(x => x.Value).ToHashSet();
+            var allIngredients = input.SelectMany(x => x.Ingredients).Where(x => !possibleIngredients.Contains(x)).ToList();
+
+            return allIngredients.Count;
+        }
+    }
+}
