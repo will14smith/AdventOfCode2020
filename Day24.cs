a2b55f49f8ed1598a31bc010a6b72fe695460375
Will Smith <will@toxon.co.uk>
2020-12-24 06:45:13 +0000

    Day24 Part2

--- Day24.cs
+++ Day24.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode2020.Utilities;
 using FluentAssertions;
 using Xunit;
@@ -9,7 +10,17 @@ namespace AdventOfCode2020
 {
     public partial class Day24 : Test
     {
-        private const string Sample = "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew\n"; 
+        private const string Sample = "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew\n";
+
+        private static readonly ReadOnlyMemory<(int, int)> Offsets = new[]
+        {
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 0),
+            (1, -1),
+            (0, -1),
+        };
         
         public Day24(ITestOutputHelper output) : base(24, output) { }
 
@@ -20,9 +31,54 @@ namespace AdventOfCode2020
             Run("actual", Tokenizer, Parser, SolvePart1);
         }
         
-        private int SolvePart1(Direction[][] input)
+        [Fact]
+        public void Part2()
         {
-            var tiles = new HashSet<(int, int, int)>();
+            Run("sample", Sample, Tokenizer, Parser, SolvePart2).Should().Be(2208);
+            Run("actual", Tokenizer, Parser, SolvePart2);
+        }
+        
+        private int SolvePart1(Direction[][] input) => CalculateTiles(input).Count;
+
+        private int SolvePart2(Direction[][] input)
+        {
+            var tiles = CalculateTiles(input);
+            
+            for (var day = 0; day < 100; day++)
+            {
+                var neighbours = new Dictionary<(int, int), int>();
+                foreach (var tile in tiles)
+                {
+                    for (var index = 0; index < Offsets.Span.Length; index++)
+                    {
+                        var offset = Offsets.Span[index];
+                        var neighbour = Add(tile, offset);
+
+                        if (!neighbours.TryAdd(neighbour, 1))
+                        {
+                            neighbours[neighbour] = neighbours[neighbour] + 1;
+                        }
+                    }
+                }
+
+                var next = new HashSet<(int, int)>();
+                foreach (var (neighbour, count) in neighbours)
+                {
+                    if (count == 2 || count == 1 && tiles.Contains(neighbour))
+                    {
+                        next.Add(neighbour);
+                    }
+                }                
+
+                tiles = next;
+            }
+
+            return tiles.Count;
+        }
+
+        private static HashSet<(int, int)> CalculateTiles(Direction[][] input)
+        {
+            var tiles = new HashSet<(int, int)>();
 
             foreach (var line in input)
             {
@@ -37,27 +93,10 @@ namespace AdventOfCode2020
                 }
             }
 
-            return tiles.Count;
+            return tiles;
         }
-        
-        private static (int, int, int) Calculate(IEnumerable<Direction> line)
-        {
-            var (x, y, z) = (0, 0, 0);
 
-            foreach (var direction in line)
-            {
-                (x, y, z) = direction switch
-                {
-                    Direction.East => (x-1, y+1, z),
-                    Direction.SouthEast => (x-1, y, z+1),
-                    Direction.SouthWest => (x, y-1, z+1),
-                    Direction.West => (x+1, y-1, z),
-                    Direction.NorthWest => (x+1, y, z-1),
-                    Direction.NorthEast => (x, y+1, z-1),
-                };
-            }
-
-            return (x, y, z);
-        }
+        private static (int, int) Calculate(IEnumerable<Direction> line) => line.Aggregate((0, 0), (current, direction) => Add(current, Offsets.Span[(int) direction]));
+        private static (int, int) Add((int, int) a, (int, int) b) => (a.Item1 + b.Item1, a.Item2 + b.Item2);
     }
 }
