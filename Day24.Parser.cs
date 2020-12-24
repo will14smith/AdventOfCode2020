079174598c5381cdaa63314ba33ff67688b29942
Will Smith <will@toxon.co.uk>
2020-12-24 06:28:36 +0000

    Day24 Part1

---
+++ Day24.Parser.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode2020.Utilities;
+using Superpower;
+using Superpower.Parsers;
+using Superpower.Tokenizers;
+
+namespace AdventOfCode2020
+{
+    public partial class Day24
+    {
+        private static readonly Tokenizer<TokenType> Tokenizer = new TokenizerBuilder<TokenType>()
+            .Match(Character.EqualTo('\n'), TokenType.NewLine)
+            .Ignore(Span.WhiteSpace)
+            .Match(Span.EqualTo("se"), TokenType.SouthEast)
+            .Match(Span.EqualTo("sw"), TokenType.SouthWest)
+            .Match(Span.EqualTo("ne"), TokenType.NorthEast)
+            .Match(Span.EqualTo("nw"), TokenType.NorthWest)
+            .Match(Span.EqualTo("e"), TokenType.East)
+            .Match(Span.EqualTo("w"), TokenType.West)
+            .Build();
+
+        private static readonly TokenListParser<TokenType, Direction> DirectionParser =
+            Token.EqualTo(TokenType.East).Select(_ => Direction.East)
+                .Or(Token.EqualTo(TokenType.SouthEast).Select(_ => Direction.SouthEast))
+                .Or(Token.EqualTo(TokenType.SouthWest).Select(_ => Direction.SouthWest))
+                .Or(Token.EqualTo(TokenType.West).Select(_ => Direction.West))
+                .Or(Token.EqualTo(TokenType.NorthWest).Select(_ => Direction.NorthWest))
+                .Or(Token.EqualTo(TokenType.NorthEast).Select(_ => Direction.NorthEast));
+        private static readonly TokenListParser<TokenType, Direction[]> LineParser = DirectionParser.AtLeastOnce().ThenIgnore(Token.EqualTo(TokenType.NewLine));
+        private static readonly TokenListParser<TokenType, Direction[][]> Parser = LineParser.AtLeastOnce();
+
+        private enum Direction
+        {
+            East,
+            SouthEast,
+            SouthWest,
+            West,
+            NorthWest,
+            NorthEast,
+        }
+            
+        private enum TokenType
+        {
+            East,
+            SouthEast,
+            SouthWest,
+            West,
+            NorthWest,
+            NorthEast,
+            
+            NewLine,
+        }
+    }
+}
---
+++ Day24.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode2020.Utilities;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020
+{
+    public partial class Day24 : Test
+    {
+        private const string Sample = "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew\n"; 
+        
+        public Day24(ITestOutputHelper output) : base(24, output) { }
+
+        [Fact]
+        public void Part1()
+        {
+            Run("sample", Sample, Tokenizer, Parser, SolvePart1).Should().Be(10);
+            Run("actual", Tokenizer, Parser, SolvePart1);
+        }
+        
+        private int SolvePart1(Direction[][] input)
+        {
+            var tiles = new HashSet<(int, int, int)>();
+
+            foreach (var line in input)
+            {
+                var loc = Calculate(line);
+                if (tiles.Contains(loc))
+                {
+                    tiles.Remove(loc);
+                }
+                else
+                {
+                    tiles.Add(loc);
+                }
+            }
+
+            return tiles.Count;
+        }
+        
+        private static (int, int, int) Calculate(IEnumerable<Direction> line)
+        {
+            var (x, y, z) = (0, 0, 0);
+
+            foreach (var direction in line)
+            {
+                (x, y, z) = direction switch
+                {
+                    Direction.East => (x-1, y+1, z),
+                    Direction.SouthEast => (x-1, y, z+1),
+                    Direction.SouthWest => (x, y-1, z+1),
+                    Direction.West => (x+1, y-1, z),
+                    Direction.NorthWest => (x+1, y, z-1),
+                    Direction.NorthEast => (x, y+1, z-1),
+                };
+            }
+
+            return (x, y, z);
+        }
+    }
+}
